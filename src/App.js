77272dac958746436c34753c1c2efed311dc6f00
Noman Patel <Nomanpatel39424@gmail.com>
2021-12-03 09:53:24 -0500

    adding redux

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './components/DirectoryComponent';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+import{Provider} from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 import './App.css';
-import{CAMPSITES} from './shared/campsites';
-import { thisExpression } from '@babel/types';
 
+const store = ConfigureStore();
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES
-        };
-    }
+    
     render() {
       return (
-          <div className="App">
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
-              <Directory campsites = {this.state.campsites}/>
-          </div>
+        <Provider store = {store}>
+          <BrowserRouter>
+            <div className="App">
+                <Main />
+            </div>
+          </BrowserRouter>
+        </Provider>
       );
   }
 }
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,44 +1,50 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import CampsiteInfo from './CampsiteInfoComponent';
-
-class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedCampsite: null
-        };
-    }
-
-    onCampsiteSelect(campsite) {
-        this.setState({selectedCampsite: campsite});
-    }
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
-    render() {
-        const directory = this.props.campsites.map(campsite => {
-            return (
-                <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
-                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{campsite.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
+function RenderDirectoryItem({campsite, onClick}) {
+    return (
+        <Card>
+            <Link to={`/directory/${campsite.id}`}>
+                    <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{campsite.name}</CardTitle>
+                    </CardImgOverlay>
+            </Link>
+        </Card>
+    );
+}
+
+function Directory(props) {
+    console.log(props)
+    // const directory = props.campsites.map(campsite => {
+    //     return (
+    //         <div key={campsite.id} className="col-md-5 m-1">
+    //             <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+    //         </div>
+    //     );
+    // });
+
+    const directory = <div/>
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
                 </div>
-            );
-        });
-
-        return (
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
-                    <CampsiteInfo campsite={this.CampsiteInfo(this.state.selectedCampsite)}/>
-                        
             </div>
-        );
-    }
+            <div className="row">
+                {directory}
+            </div>
+        </div>
+    );
 }
 
 export default Directory;
